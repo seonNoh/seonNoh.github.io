7f9830bef04a84ed355d405f0c84047831aad760
seonnoh <dark.pearl.nst@gmail.com>
2019-01-20 16:30:04 +0900

    another trying

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-// Initialise FlexSlider for Carousels
-$(window).load(function() {
-    $('.flexslider').flexslider({
-    animation: "slide",
-    directionNav: true,
-    touch: true
-    });
-});
