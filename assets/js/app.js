c04d306c2e6a373578b6cef4dbef38c4cf5f0be7
seonnoh <dark.pearl.nst@gmail.com>
2019-01-20 15:33:05 +0900

    add setting for flexslider lib

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+// Initialise FlexSlider for Carousels
+$(window).load(function() {
+    $('.flexslider').flexslider({
+    animation: "slide",
+    directionNav: true,
+    touch: true
+    });
+});
